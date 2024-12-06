6bfcfd93134eaa209088bd557093d00fedf9873c
Adam <Adamalzuheire1255@gmail.com>
2024-12-06 12:54:55 +0100

    Ny test fil til projektet

--- vision/Test_grader_til_radianer.py
+++ vision/Test_grader_til_radianer.py
@@ -20,40 +20,40 @@ def main():
 
         # Positioner i radianer
         position1 = [
-            grader_til_radianer( 134),
-            grader_til_radianer(-36.09),
-            grader_til_radianer(27.55),
-            grader_til_radianer(-83.57),
-            grader_til_radianer(-88.17),
-            grader_til_radianer(333.61)
+            grader_til_radianer(132),
+            grader_til_radianer(-20.42),
+            grader_til_radianer(-17.94),
+            grader_til_radianer(-53.79),
+            grader_til_radianer(-86.37),
+            grader_til_radianer(333.03),
         ]
 
         position2 = [
             grader_til_radianer(109.20),
-            grader_til_radianer( -80.10),
-            grader_til_radianer( 98.40),
-            grader_til_radianer(-110.38),
+            grader_til_radianer(-82.60),
+            grader_til_radianer(87.71),
+            grader_til_radianer(-97.17),
             grader_til_radianer(-89.53),
-            grader_til_radianer(309.77),
+            grader_til_radianer(309.78),
         ]
 
         position3 = [
-            grader_til_radianer(171.79),
-            grader_til_radianer(-49.67),
-            grader_til_radianer(52.06),
-            grader_til_radianer(-93.42),
+            grader_til_radianer(171.81),
+            grader_til_radianer(-48.82),
+            grader_til_radianer(38.53),
+            grader_til_radianer(-80.75),
             grader_til_radianer(-89.06),
-            grader_til_radianer(11.92),
+            grader_til_radianer(11.94),
 
         ]     
 
         position4 = [
-            grader_til_radianer(172.08),
-            grader_til_radianer(-109.55),
-            grader_til_radianer(123.86),
-            grader_til_radianer(-104.70),
-            grader_til_radianer(-91.92),
-            grader_til_radianer(11.92),
+            grader_til_radianer(172.05),
+            grader_til_radianer(-111.37),
+            grader_til_radianer(110.94),
+            grader_til_radianer(-89.70),
+            grader_til_radianer(-91.91),
+            grader_til_radianer(11.9),
 
         ]     
 
@@ -61,27 +61,27 @@ def main():
         # Bevægelser med datalogning
         print("Bevæger robot til første position...")
         rtde_c.moveJ(position1, 1.2, 0.25)  # Udfør bevægelse til position 1
-        time.sleep(7)  # Vent til bevægelse er færdig
+        time.sleep(3)  # Vent til bevægelse er færdig
         tcp_pose1 = rtde_r.getActualTCPPose()  # Hent TCP-position
         print(f"TCP Position efter første bevægelse: {tcp_pose1}")
 
         print("Bevæger robot til anden position...")
         rtde_c.moveJ(position2, 1.2, 0.25)  # Udfør bevægelse til position 2
-        time.sleep(7)
+        time.sleep(3)
         tcp_pose2 = rtde_r.getActualTCPPose()
         print(f"TCP Position efter anden bevægelse: {tcp_pose2}")
 
        #Bevægelse til trejde position
         print("Bevæger robot til tredje position...")
         rtde_c.moveJ(position3, 1.2, 0.25)  # Udfør bevægelse til position 3
-        time.sleep(7)
+        time.sleep(3)
         tcp_pose3 = rtde_r.getActualTCPPose()
         print(f"TCP Position efter tredje bevægelse: {tcp_pose3}") 
 
         #Bevægelse til fjerde position
         print("Bevæger robot til fjerde position...")
         rtde_c.moveJ(position4, 1.2, 0.25)  # Udfør bevægelse til position 3
-        time.sleep(7)
+        time.sleep(3)
         tcp_pose4 = rtde_r.getActualTCPPose()
         print(f"TCP Position efter fjerde bevægelse: {tcp_pose4}")
 
